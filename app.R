2fa4a4537b48e56f9adb9120d0b06c95bfe26e05
Marcelo Albuquerque <marceloralbuquerque@gmail.com>
2020-04-06 00:25:22 -0300

    Add shinyapps deploy config

---
+++ app.R
@@ -0,0 +1,7 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp(appName=covid19viz)
+# Or use the blue button on top of this file
+
+pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+options( "golem.app.prod" = TRUE)
+coronavirusbrazil::run_app()
