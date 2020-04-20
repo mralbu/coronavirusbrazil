7dcb432ad0810386d7b212abbbaf3a6ebf13ec74
Marcelo Albuquerque <marceloralbuquerque@gmail.com>
2020-04-20 19:22:30 -0300

    Update Datasets

--- R/app_server.R
+++ R/app_server.R
@@ -37,98 +37,106 @@ app_server = function(input, output) {
   })
 
   output$map_cases_countries = mapview::renderMapview(
-    mapview::mapview(coronavirusbrazil::spatial_world, zcol="cases", cex="log_cases", alpha=1,
+    mapview::mapview(coronavirusbrazil::spatial_world, zcol="log_cases", cex="log_cases", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles(),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Casos",
                      label=paste0(coronavirusbrazil::spatial_world$country,
                                   ": ", coronavirusbrazil::spatial_world$cases))
   )
 
   output$map_deaths_countries = mapview::renderMapview(
-    mapview::mapview(coronavirusbrazil::spatial_world %>% dplyr::filter(deaths > 0), zcol="deaths", cex="log_deaths", alpha=1,
+    mapview::mapview(coronavirusbrazil::spatial_world %>% dplyr::filter(deaths > 0), zcol="log_deaths", cex="log_deaths", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles(),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Mortes",
                      label=paste0((coronavirusbrazil::spatial_world %>% dplyr::filter(deaths > 0))$country,
                                   ": ", (coronavirusbrazil::spatial_world %>% dplyr::filter(deaths > 0))$deaths))
   )
 
   output$map_cases_states = mapview::renderMapview(
-    mapview::mapview(coronavirusbrazil::spatial_br_states, zcol="cases", cex="log_cases", alpha=1,
+    mapview::mapview(coronavirusbrazil::spatial_br_states, zcol="log_cases", cex="log_cases", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles() %>%
                        leaflet::setView(-60, -18, zoom=3),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Casos",
                      label=paste0(coronavirusbrazil::spatial_br_states$uf,
                                   ": ", coronavirusbrazil::spatial_br_states$cases))
   )
 
   output$map_deaths_states = mapview::renderMapview(
-    mapview::mapview(coronavirusbrazil::spatial_br_states, zcol="deaths", cex="log_deaths", alpha=1,
+    mapview::mapview(coronavirusbrazil::spatial_br_states, zcol="log_deaths", cex="log_deaths", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles() %>%
                        leaflet::setView(-60, -18, zoom=3),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Mortes",
                      label=paste0(coronavirusbrazil::spatial_br_states$uf,
                                   ": ", coronavirusbrazil::spatial_br_states$deaths))
   )
 
   output$map_cases_cities = mapview::renderMapview(
-    mapview::mapview((coronavirusbrazil::spatial_br_cities %>% dplyr::filter(cases > 0)), zcol="cases", cex="log_cases", alpha=1,
+    mapview::mapview((coronavirusbrazil::spatial_br_cities %>% dplyr::filter(cases > 0)), zcol="log_cases", cex="log_cases", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles() %>%
                        leaflet::setView(-60, -18, zoom=3),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Casos",
                      label=paste0((coronavirusbrazil::spatial_br_cities %>% dplyr::filter(cases > 0))$city,
                                   ": ", (coronavirusbrazil::spatial_br_cities %>% dplyr::filter(cases > 0))$cases))
   )
 
   output$map_deaths_cities = mapview::renderMapview(
-    mapview::mapview(coronavirusbrazil::spatial_br_cities %>% dplyr::filter(deaths > 0), zcol="deaths", cex="log_deaths", alpha=1,
+    mapview::mapview(coronavirusbrazil::spatial_br_cities %>% dplyr::filter(deaths > 0), zcol="log_deaths", cex="log_deaths", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles() %>%
                        leaflet::setView(-60, -18, zoom=3),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Mortes",
                      label=paste0((coronavirusbrazil::spatial_br_cities %>% dplyr::filter(deaths > 0))$city, ": ", (coronavirusbrazil::spatial_br_cities %>% dplyr::filter(deaths > 0))$deaths))
   )
 
   output$map_cases_rj = mapview::renderMapview(
-    mapview::mapview(coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, cases > 0), zcol="cases", cex="log_cases", alpha=1,
+    mapview::mapview(coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, cases > 0), zcol="log_cases", cex="log_cases", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles(),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Casos",
                      label=paste0((coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, cases > 0))$neighborhood, ": ", (coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, cases > 0))$cases))
   )
 
   output$map_deaths_rj = mapview::renderMapview(
-    mapview::mapview(coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, deaths > 0), zcol="deaths", cex="log_deaths", alpha=1,
+    mapview::mapview(coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, deaths > 0), zcol="log_deaths", cex="log_deaths", alpha=1,
                      col.regions=viridisLite::viridis(n=256,
                                                       alpha=0.1,
                                                       direction=1),
                      map = leaflet::leaflet() %>% leaflet::addTiles(),
                      popup=NULL,
+                     legend=FALSE,
                      layer.name="Mortes",
                      label=paste0((coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, deaths > 0))$neighborhood, ": ", (coronavirusbrazil::spatial_rj_neighborhoods %>% dplyr::filter(lon < -30, deaths > 0))$deaths))
   )
